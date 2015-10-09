17457f0b6dbaa47d834a3167c4cc64a13e22f426
Sergiy Govorukhin <govorukhin@gmail.com>
2015-10-10 00:30:49 +0300

    Initial commit

---
+++ src/main/HomeTask1.java
@@ -0,0 +1,12 @@
+package main;
+
+/**
+ * @author Sergiy Govorukhin
+ * @version 1.0
+ */
+
+public class HomeTask1 {
+    public static void main(String[] args) {
+        new TaskManager();
+    }
+}
