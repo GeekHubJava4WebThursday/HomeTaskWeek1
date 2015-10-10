d55922e3083b5234ced308024d435d05cdb1095d
Nicholas.Morhun <Nicksbox78@gmail.com>
2015-10-10 22:55:08 +0300

    Added factorial calculator

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import factorial.FactorialCalculator;
 import fibonacci.FibonacciSequencePrinter;
 
 public class Main {
@@ -10,9 +11,17 @@ public class Main {
         // test Fibonacci's number sequence
         FibonacciSequencePrinter fibonacciSequencePrinter = new FibonacciSequencePrinter(10);
         fibonacciSequencePrinter.print();
-        System.out.println("==================================================");
+        System.out.println("--------------------------------------------------");
         fibonacciSequencePrinter.setLengthOfSequence(200);
         fibonacciSequencePrinter.print();
+
+        System.out.println("==================================================");
+
+        // test factorial calculator
+        FactorialCalculator factorialCalculator = new FactorialCalculator();
+        System.out.println(factorialCalculator.calculate(5));
+        System.out.println(factorialCalculator.calculate(50));
+
     }
 
 }
