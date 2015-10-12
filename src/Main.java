e04febe3f55b94e24d4ec98c37cca87dcf4f1618
Vadim Kostenko <er.coooool@gmail.com>
2015-10-12 18:50:02 +0300

    third commit

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String argv[]) {
         System.out.println(Fibbonachi.findSequence(10));
         System.out.println(Fibbonachi.findNumber(4));
-        System.out.println(Factorial.find(10));
+        System.out.println(Factorial.find(8));
         System.out.println(Numerals.getWord(0));
     }
 }
