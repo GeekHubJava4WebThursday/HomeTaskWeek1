cf851d161df2baafc3c5876ed5e4d2197fcf2b61
Sergiy Govorukhin <govorukhin@gmail.com>
2015-10-14 00:56:54 +0300

    Separate method created  for checking "number" parameter

--- src/main/Factorial.java
+++ src/main/Factorial.java
@@ -10,15 +10,27 @@ public class Factorial {
     public static final byte MAX_NUMBER = 20;
 
     /**
+     *  Checks the number
+     *  @param number - must be between 0 and MAX_NUMBER
+     */
+    private static boolean isNumberCorrect(long number) {
+        if (number > MAX_NUMBER) {
+            System.out.println("Number must be less then " + MAX_NUMBER + "!");
+            return false;
+        } else if (number < 0) {
+            System.out.println("Number must be positive!");
+            return false;
+        } else
+            return true;
+    }
+
+    /**
      *  Recursive method
      */
     public static long getFactorialRecursive(long number) {
-        if (number > MAX_NUMBER) {
-            System.out.println("Number must be less then " + MAX_NUMBER + "!");
-        } else if (number < 0) {
-            System.out.println("Number must be positive!");
-        } else
-            return (number > 1) ? number * getFactorialRecursive(number - 1): 1;
+        if (isNumberCorrect(number)) {
+            return (number > 1) ? number * getFactorialRecursive(number - 1) : 1;
+        }
         return 0;
     }
 
@@ -26,11 +38,7 @@ public class Factorial {
      *  Classic naive method
      */
     public static long getFactorial(long number) {
-        if (number > MAX_NUMBER) {
-            System.out.println("Number must be less then " + MAX_NUMBER + "!");
-        } else if (number < 0) {
-            System.out.println("Number must be positive!");
-        } else {
+        if (isNumberCorrect(number)) {
             long result = 1;
             for (long i = 2; i <= number; i++) {
                 result *= i;
